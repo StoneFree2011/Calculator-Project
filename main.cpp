f550149108c37035941879efd79470df56d698c0
StoneFree2011 <greendayarseniy@gmail.com>
2023-11-26 20:35:56 +0300

    Скелет программы

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "funcs.h"
+using namespace std;
+int main()
+{
+	int n1, n2;
+	char oper;
+	cout << "Input 1st number: " << endl;
+	cin >> n1;
+	cout << "Choose operator: \n+ (sum)\n- (substraction)\n/ (division)\n * multiplication\n";
+	cin >> oper;
+	cout << "Input 2nd number: " << endl;
+	cin >> n2;
+	cout << "Please wait! Functions are developing!" << endl;
+	system("pause");
+	return 0;
+}
